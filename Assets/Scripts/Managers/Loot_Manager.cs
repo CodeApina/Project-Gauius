1f5a2bb7ad70cc0e3888e51786e5ff3f665249b9
Henri Sarén <henri.saren@gmail.com>
2023-06-21 09:44:48 +0300

    Complete loot generation function
    Finished loot generation function in loot manager, it now correctly
    returns a list of items generated

--- Assets/Scripts/Managers/Loot_Manager.cs
+++ Assets/Scripts/Managers/Loot_Manager.cs
@@ -14,6 +14,7 @@ public class Loot_Manager : MonoBehaviour
     
     public List<Item_Stats> Generate_Loot(float monster_level)
     {
+        List<Item_Stats> loot = new List<Item_Stats>();
         float min_loot_amount = 1 * monster_level / 10;
         float max_loot_amount = 10 * monster_level / 10;
         if (min_loot_amount < 1)
@@ -23,9 +24,9 @@ public class Loot_Manager : MonoBehaviour
         float loot_amount = Random.Range(min_loot_amount, max_loot_amount);
         for (int i = 0; i < loot_amount; i++)
         {
-            item.Generate_Item(monster_level);
+            loot.Add(item.Generate_Item(monster_level));
         }
-        return null;
+        return loot;
     }
     void Awake()
     {
