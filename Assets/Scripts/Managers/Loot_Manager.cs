be3eb20bc534e47e050247bb7a0a73a35c0bd76d
Henri Sarén <henri.saren@gmail.com>
2023-07-20 14:12:02 +0300

    Fix Loot creation
    Loot creation script no longer creates an extra empty gameobject
    Also loot amount calculation was fixed, previously only created 1 item
    until level 11

--- Assets/Scripts/Managers/Loot_Manager.cs
+++ Assets/Scripts/Managers/Loot_Manager.cs
@@ -8,6 +8,7 @@ public class Loot_Manager : MonoBehaviour
 {
     public static Loot_Manager Instance;
     public GameObject ui_element;
+    public GameObject item_base;
     public List<Weapon_Scriptable_Object> weapon_models;
     public List<Armor_Scriptable_Object> armor_models;
     public List<Item_Modifiers_Scriptable_Object> modifiers;
@@ -15,20 +16,20 @@ public class Loot_Manager : MonoBehaviour
     public List<GameObject> Generate_Loot(float monster_level)
     {
         List<GameObject> loot = new List<GameObject>();
-        float min_loot_amount = Mathf.Ceil(1 * monster_level / 10);
-        float max_loot_amount = Mathf.Ceil(5 * monster_level / 10);
+        float min_loot_amount = Mathf.Round(1 * (1 + monster_level / 10));
+        float max_loot_amount = Mathf.Round(5 * (1+ monster_level / 10));
         float loot_amount = Random.Range(min_loot_amount, max_loot_amount);
         for (int i = 0; i < loot_amount; i++)
         {
-            GameObject item_being_generated = new GameObject();
-            Instantiate(item_being_generated, new Vector3(9999f, 9999f, 9999f), Quaternion.identity);
+            GameObject item_being_generated = Instantiate(item_base);
             item_being_generated.AddComponent<Item>();
             item_being_generated.GetComponent<Item>().stats = item_being_generated.GetComponent<Item>().Generate_Item(monster_level);
             item_being_generated.name = item_being_generated.GetComponent<Item>().stats.name;
-            var ui = Instantiate(ui_element);
+            GameObject ui = Instantiate(ui_element);
             ui.transform.parent = item_being_generated.transform;
             ui.GetComponent<Item_UI>().image.color = item_being_generated.GetComponent<Item>().stats.color;
             ui.GetComponent<Item_UI>().name_field.text = item_being_generated.GetComponent<Item>().stats.name;
+            item_being_generated.SetActive(false);
             loot.Add(item_being_generated);
         }
         return loot;
@@ -40,6 +41,7 @@ public class Loot_Manager : MonoBehaviour
             
             current_item.transform.position = enemy.transform.position;
             current_item.transform.rotation = Quaternion.identity;
+            current_item.SetActive(true);
         }
     }
     void Awake()
@@ -53,5 +55,6 @@ public class Loot_Manager : MonoBehaviour
             Instance = this;
         }
         DontDestroyOnLoad(this);
+        item_base = new GameObject();
     }
 }
